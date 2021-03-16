1066ad5a0e035067493761c445b50a857d7e8a86
Trevor <tside777@gmail.com>
2021-03-16 11:24:26 -0400

    Added buttons and events to game for UI play

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const playButton = document.querySelector('#play');
+
+const rockButton = document.createElement('button');
+rockButton.textContent="Rock";
+rockButton.id='ROCK';
+
+const paperButton = document.createElement('button');
+paperButton.textContent="Paper"
+paperButton.id='PAPER';
+
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent="Scissors"
+scissorsButton.id='SCISSORS';
+
+const content = document.querySelector('.main-container');
+
+let playerScore = 0;
+let computerScore = 0;
+
+playButton.addEventListener('click', () => {
+    content.removeChild(playButton);
+    content.appendChild(rockButton);
+    content.appendChild(paperButton);
+    content.appendChild(scissorsButton);
+    game();
+});
+
+
+
+
+
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -19,19 +51,8 @@ function computerPlay() {
     return RPS;
 }
 
-function playerPlay() {
-    let playerSelection = prompt("Choose Rock, Paper, or Scissors").toUpperCase();
-
-    while(playerSelection !== "ROCK"  && playerSelection !== "PAPER" && playerSelection !== "SCISSORS") {
-        playerSelection = prompt("Oops, try again! Choose Rock, Paper, or Scissors").toUpperCase();
-    }
-
-    return playerSelection;
-}
-
 
 function playRound(playerSelection, computerSelection) {
-    
     if(playerSelection === computerSelection) {
         return -1;
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
@@ -50,40 +71,46 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
+function play(e) {
+    let computerSelection = computerPlay()
+    let result = playRound(e.target.id, computerSelection);
+
+    if (result == 1) {
+        console.log(`You Win! ${e.target.id} beats ${computerSelection}!`);
+        playerScore += 1;
+    } else if (result == 0) {
+        console.log(`You Lose! ${computerSelection} beats ${e.target.id}!`);
+        computerScore += 1;
+    } else {
+        console.log("Tie!");
+    }
+    console.log(`Player: ${playerScore} \t Computer: ${computerScore}`);
+
+    if (playerScore === 5 || computerScore === 5) {
+        endGame(playerScore, computerScore);
+    }
+}
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let i = 0
-    while (i < 5) {
-
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result == 1) {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
-            playerScore += 1;
-            i += 1;
-        } else if (result == 0) {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`);
-            computerScore += 1;
-            i += 1;
-        } else {
-            console.log("Tie Game!");
-        }
-        
-        console.log(`Player: ${playerScore} \t Computer: ${computerScore}`);
+    playerScore = 0;
+    computerScore = 0;
+    const buttons = document.querySelectorAll('.main-container button');
+    buttons.forEach(button => {
+        button.addEventListener('click', play)
+        });
     }
 
-
+function endGame(playerScore, computerScore) {
+    const buttons = document.querySelectorAll('.main-container button');
     if (playerScore > computerScore) {
         console.log(`You Win ${playerScore} - ${computerScore}`);
     } else {
         console.log(`You Lose ${playerScore} - ${computerScore}`);
     }
+    buttons.forEach(button => {
+        button.removeEventListener('click', play);
+        content.removeChild(button);
+    });
+
+    content.appendChild(playButton);
 }
-
-
-game();
